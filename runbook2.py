e9b267114fd457fdc90686e6dbbff07fe9e3305f
Gandalf <vodovilich@gmail.com>
2023-04-01 09:04:25 -0400

    add runbook2

---
+++ runbook2.py
@@ -0,0 +1,13 @@
+from nornir import InitNornir
+from nornir_scrapli.tasks import send_config
+from nornir_utils.plugins.functions import print_result
+
+nr = InitNornir(config_file="config.yaml")
+
+def random_config(task):
+
+ #   task.run(task=send_config, config="ntp server 1.1.1.1") - not cool, better use f-strings for substitution:
+    task.run(task=send_config, config=f"ntp server {task.host['ntp_server']}")
+
+results = nr.run(task=random_config)
+print_result(results)
